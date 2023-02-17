3a3813cd3602d6e35f556a876268aeb29c876da3
Jan Kohlbach <jan@dorfjungs.com>
2023-02-17 21:31:49 +0000

    ✨ add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       title: 'canary islands',
       link: [
         // { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        // { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' },
+        { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }
         // { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
         // { rel: 'manifest', href: '/manifest.webmanifest' }
       ],
