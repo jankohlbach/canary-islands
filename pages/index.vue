92aff9a5efc303c3f50b22cedcc626a086aeb097
Jan Kohlbach <jan@dorfjungs.com>
2023-06-09 13:37:12 +0200

    ⬆️ upgrade dependencies

--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 onMounted(() => {
   window.addEventListener('resize', () => debounce(ScrollTrigger.refresh()))
 
-  const lenis = new Lenis({
-    duration: 1.2,
-    easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-    direction: 'vertical', // vertical, horizontal
-    gestureDirection: 'vertical', // vertical, horizontal, both
-    smooth: true,
-    mouseMultiplier: 1,
-    smoothTouch: false,
-    touchMultiplier: 2,
-    infinite: false
-  })
+  const lenis = new Lenis()
 
   function raf (time: any) {
     lenis.raf(time)
